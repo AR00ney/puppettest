4adde308425f4934d780a7d54aa36c7037322e5d
Alex <a.rooneysemail@gmail.com>
2023-05-11 06:50:45 +0100

    test

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,6 +1,9 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
+const ribbitsURL ='https://sologenic.org/trade?market=5249424249545300000000000000000000000000%2BrPmb5BPBAbE9jmNaFXNPH5kZEPDpRxaY77%2FXRP&network=mainnet'
+
+
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
     args: [
@@ -17,27 +20,34 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto(ribbitsURL)
+      //"https://developer.chrome.com/");
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    const ribbitsPage = await page.evaluate(() => {
+      const price = document.querySelector(".price").innerText;
+      return price;
+  });
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    // // Set screen size
+    // await page.setViewport({ width: 1080, height: 1024 });
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    // // Type into search box
+    // await page.type(".search-box__input", "automate beyond recorder");
+
+    // // Wait and click on first result
+    // const searchResultSelector = ".search-box__link";
+    // await page.waitForSelector(searchResultSelector);
+    // await page.click(searchResultSelector);
+
+    // // Locate the full title with a unique string
+    // const textSelector = await page.waitForSelector(
+    //   "text/Customize and automate"
+    // );
+    // const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
+    const logStatement = `The result is ${ribbitsPage}`;
     console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
